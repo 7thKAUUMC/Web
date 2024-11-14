292b18e8321d865ba6580fd22ac41da2186d50f9
haerxeong <haeryeong04@naver.com>
2024-11-14 17:40:37 +0900

    mission2: infinite scroll

---
+++ 7th/mission2/src/pages/Upcoming/upcoming.jsx
@@ -0,0 +1,47 @@
+import Card from "../../components/Card/card.jsx";
+import * as S from "../../components/Card/card.style.js";
+import UseGetInfiniteMovies from "../../hooks/queries/useGetInfiniteMovies.js";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
+import CardListSkeleton from "../../components/Card/Skeleton/card-list-skeleton.jsx";
+
+const UpcomingMoviesPage = () => {
+  const {
+    data: movies,
+    isFetching,
+    hasNextPage,
+    fetchNextPage,
+  } = UseGetInfiniteMovies("upcoming");
+
+  const { ref, inView } = useInView({
+    threshold: 0,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      !isFetching && hasNextPage && fetchNextPage();
+    }
+  }, [inView, isFetching, hasNextPage, fetchNextPage]);
+
+  console.log(movies);
+
+  return (
+    <>
+      <S.CardList>
+        {movies?.pages
+          ?.map((page) => page.results)
+          ?.flat()
+          ?.map((movie, _) => (
+            <Card key={movie.id} movie={movie} />
+          ))}
+        {isFetching && <CardListSkeleton number={20} />}
+      </S.CardList>
+      <S.LoadingContainer ref={ref}>
+        {isFetching && <ClipLoader color="white" />}
+      </S.LoadingContainer>
+    </>
+  );
+};
+
+export default UpcomingMoviesPage;
